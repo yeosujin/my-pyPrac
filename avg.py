542821489698daaf28fed716afa705375812ff36
yeosujin <58133978+yeosujin@users.noreply.github.com>
2024-04-07 14:24:57 +0900

    Add solution2 of avg.py
    For 99CLUB's assignment, I add another solution of Calculating Average of Array.

--- avg.py
+++ avg.py
@@ -12,11 +12,27 @@ arr	return
 '''
 
 
-
+#1
 def solution(arr):
-    answer = 0
+    
     if 1<= len(arr) <= 100:                     #제한1
         for i in arr: 
             if -10000<=i<=10000:                #제한2
                 answer = sum(arr) / len(arr)    #평균값 구하기
                 return answer
+
+
+#2
+def solution(arr):
+    answer = 0
+    #제한1
+    if 1<= len(arr) <= 100:
+        #arr의 갯수만큼 반복
+        for i in range(len(arr)): 
+            #제한2
+            if -10000<=i<=10000:
+                #arr배열의 수를 answer에 넣기
+                answer += arr[i]
+        #arr의 값들이 다 더해진 answer를 arr의 길이만큼 나누어 평균 계산
+        answer = answer / len(arr)
+        return answer
